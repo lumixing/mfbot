a170135e9a808764cf9d842cc71399df28e39486
lumix <over100stars@gmail.com>
2021-05-23 15:41:38 +0300

    added ;github

---
+++ commands/github.js
@@ -0,0 +1,11 @@
+module.exports.run = async (msg, args) => {
+  msg.channel.send("mfbot is open source! https://github.com/lumixing/mfbot");
+}
+module.exports.meta = {
+	name: "github",
+	aliases: ["git", "gh"],
+	description: "shows the github repo for mfbot",
+  usage: "github",
+	argsRequired: false,
+	category: "utility"
+}
