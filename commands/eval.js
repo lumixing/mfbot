4e8ffb61ecf5e2849b5e9f389e19d86f7e7966e0
lumix <over100stars@gmail.com>
2021-05-30 01:20:17 +0300

    improved ;eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,12 +1,33 @@
 const error = require("../functions/error");
+const react = require("../functions/react");
 
 module.exports.run = async (msg, args) => {
+  const bot = msg.client;
+  const user = msg.author;
+  const member = msg.member;
+  const channel = msg.channel;
+  const guild = msg.guild;
+
 	try {
-		let result = eval(args.join(" "));
-		if (typeof result !== "undefined") {
-			msg.channel.send(result)
-				.catch((err) => {});
-		}
+		let result = await eval(args.join(" "));
+    result = JSON.stringify(result, null, 2);
+    msg.channel.send(result, { code: "json" })
+      .catch(async (err) => {
+        if (String(err).match(/Must be 2000 or fewer in length/g)) {
+          react(msg, "message is over 2000 characters, send it splited?", ["✅","❌"], msg.author, 10000, (reaction) => {
+            switch(reaction.emoji.name) {
+              case "✅":
+                msg.channel.send(result, { code: "json", split: true });
+                break;
+              case "❌":
+                msg.delete().catch(() => 0);
+                break;
+            }
+          }, (c) => {
+            if (c.size === 0) msg.channel.send("didnt decide anything...")
+          });
+        }
+      });
 	}
 	catch (err) {
     error(msg, `an error occured\n${String(err)}`);
@@ -15,7 +36,7 @@ module.exports.run = async (msg, args) => {
 }
 module.exports.meta = {
 	name: "eval",
-	aliases: [],
+	aliases: ["ev"],
 	description: "evaluates code",
   usage: "eval [code]",
 	argsRequired: true,
