1357406861e729b452134395fdc3257a848025b4
lumix <over100stars@gmail.com>
2021-06-01 16:25:18 +0300

    improved ;log

--- commands/log.js
+++ commands/log.js
@@ -1,9 +1,13 @@
 const error = require("../functions/error");
 
 module.exports.run = async (msg, args) => {
+	const clear = console.clear();
+	const line = console.log("==========================================================");
+
 	try {
 		let result = eval(args.join(" "));
 		console.log(result);
+		msg.react("✅");
 	}
 	catch (err) {
 		error(msg, `an error occured\n${String(err)}`);
