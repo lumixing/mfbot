4f87c09504dde6d13b0d8c70e83f90c6be5a9448
lumix <over100stars@gmail.com>
2021-06-14 16:46:07 +0300

    improved ;millis

--- commands/millis.js
+++ commands/millis.js
@@ -1,15 +1,35 @@
-const ta = require("time-ago");
+const { WHITE } = require("../resources/colors.json");
 const error = require("../functions/error");
+const ms = require("humanize-duration");
 
 module.exports.run = async (msg, args) => {
 	let date = new Date(parseInt(args[0]));
 
 	if (!args.length) {
 		date = new Date();
-		return msg.channel.send(`${date.getTime()}\n${date.toLocaleString()}`);
 	}
 
-	return msg.channel.send(`${date.getTime()}\n${date.toLocaleString()}\n${ta.ago(date)}`);
+	if (isNaN(date.getTime())) {
+		return error(msg, "that is not a valid date, enter a millis number");
+	}
+
+	let diffDate = new Date(new Date() - date);
+	let agoOrFromNow = Math.sign(diffDate.getTime()) === 1 ? "ago" : "from now";
+	let options = { largest: 2, round: true };
+
+	let descriptionArray = [
+		`**date**: ${date.toLocaleString()}`,
+		`**ms**: ${date.getTime()}`
+	];
+
+	if (args.length) descriptionArray.push(`**time ${agoOrFromNow}**: ${ms(diffDate, options)}`);
+
+	msg.channel.send({
+		embed: {
+			color: WHITE,
+			description: descriptionArray.join("\n")
+		}
+	});
 }
 module.exports.meta = {
 	name: "millis",
