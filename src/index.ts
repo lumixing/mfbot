d27dc0829cae179c1e5e098e4ec3f1ec288ff09e
lumix <over100stars@gmail.com>
2022-07-05 01:41:27 +0300

    updated logging at start

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,18 @@ import "./lib/setup";
 import { LogLevel, SapphireClient } from "@sapphire/framework";
 
 const client = new SapphireClient({
-    defaultPrefix: "!",
-    regexPrefix: /^(hey +)?bot[,! ]/i,
+    defaultPrefix: ";",
     caseInsensitiveCommands: true,
-    logger: {
-        level: LogLevel.Debug
-    },
+    logger: { level: LogLevel.Debug },
     shards: "auto",
-    intents: [
-        "GUILDS",
-        "GUILD_MEMBERS",
-        "GUILD_BANS",
-        "GUILD_EMOJIS_AND_STICKERS",
-        "GUILD_VOICE_STATES",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS"
-    ]
+    intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
 const main = async () => {
     try {
-        client.logger.info("Logging in");
+        client.logger.info("Logging in...");
         await client.login();
-        client.logger.info("logged in");
+        client.logger.info("Logged in!");
     } catch (error) {
         client.logger.fatal(error);
         client.destroy();
