7ef2caca8fc862e1af60a62ec70e72b43bf9d8e2
lumix <over100stars@gmail.com>
2022-07-06 22:25:15 +0300

    updated ;guessemoji code

--- src/commands/games/guessemoji.ts
+++ src/commands/games/guessemoji.ts
@@ -9,48 +9,53 @@ import { emojis } from "../../assets/emojis.json";
     aliases: ["ge"]
 })
 export class UserCommand extends Command {
+    private guildID!: string;
+    private member!: GuildMember;
+    private emoji!: string;
+
     public async messageRun(message: Message) {
-        // im so sorry for this
-        let streak = container.guessEmojiStreak.has(message.guild?.id!)
-            ? container.guessEmojiStreak.get(message.guild?.id!)
-            : container.guessEmojiStreak.set(message.guild?.id!, 0).get(message.guild?.id!);
+        this.guildID = message.guild?.id!;
+        this.member = message.member!;
+
+        // im so sorry for everything oh god remind me to rewrite this
+        let streak = container.guessEmojiStreak.has(this.guildID)
+            ? container.guessEmojiStreak.get(this.guildID)
+            : container.guessEmojiStreak.set(this.guildID, 0).get(this.guildID);
 
         const randomEmoji = emojis[~~(Math.random() * emojis.length)];
         const emojiMessage = await reply(message, `:${randomEmoji}:`);
+        this.emoji = randomEmoji;
 
         const filter = (m: Message) => m.author.id === message.author.id;
         try {
             const collection = await message.channel.awaitMessages({ filter, time: 15000, max: 1, errors: ["time"] });
             const replyMessage = collection.first()?.content.toLowerCase().replaceAll(" ", "_");
 
-            if (replyMessage === randomEmoji) {
-                container.guessEmojiStreak.set(message.guild?.id!, streak! + 1);
-                return reply(emojiMessage, { embeds: [this.returnEmbed(message.member!, streak!, randomEmoji, "success")] });
-            }
-
-            container.guessEmojiStreak.set(message.guild?.id!, 0);
-            return reply(emojiMessage, { embeds: [this.returnEmbed(message.member!, streak!, randomEmoji, "fail")] });
-        } catch (err: any) {
-            container.guessEmojiStreak.set(message.guild?.id!, 0);
-            return reply(emojiMessage, { embeds: [this.returnEmbed(message.member!, streak!, randomEmoji, "timeout")] });
-        }
+            if (replyMessage === randomEmoji) return this.setStreakAndReply(streak! + 1, emojiMessage, "success");
+            this.setStreakAndReply(0, emojiMessage, "fail");
+        } catch (err: any) { this.setStreakAndReply(0, emojiMessage, "timeout"); }
     }
 
-    private returnEmbed(member: GuildMember, streak: number, emoji: string, type: "success" | "fail" | "timeout") {
+    private setStreakAndReply(streak: number, emojiMessage: Message, type: "success" | "fail" | "timeout") {
+        container.guessEmojiStreak.set(this.guildID, streak);
+        reply(emojiMessage, { content: this.member.toString(), embeds: [this.returnEmbed(streak!, type)] });
+    }
+
+    private returnEmbed(streak: number, type: "success" | "fail" | "timeout") {
         let title;
 
         if (type === "success") title = `correct emote!`;
-        else if (type === "fail") title = `wrong emote!\nit was \`${emoji}\``;
-        else title = `ran out of time!\nit was \`${emoji}\``;
+        else if (type === "fail") title = `wrong emote!\nit was \`${this.emoji}\``;
+        else title = `ran out of time!\nit was \`${this.emoji}\``;
 
         return {
             color: type === "success" ? 0x2ecc71 : 0xe74c3c,
             author: {
-                name: member.nickname! ?? member.user.username,
-                icon_url: member?.user.avatarURL()!
+                name: this.member.nickname! ?? this.member.user.username,
+                icon_url: this.member?.user.avatarURL()!
             },
             title,
-            description: type === "success" ? `current streak: \`${streak + 1}\`` : `lost streak: \`${streak}\``
+            description: `${type === "success" ? "current" : "lost"} streak: \`${streak}\``
         };
     }
 }
