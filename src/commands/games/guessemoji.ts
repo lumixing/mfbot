9f6db98f072ff178ef33f33e8dc1e836fcd1c3bc
lumix <over100stars@gmail.com>
2022-07-09 13:22:45 +0300

    updated ;guessemoji code

--- src/commands/games/guessemoji.ts
+++ src/commands/games/guessemoji.ts
@@ -1,8 +1,9 @@
 import { ApplyOptions } from "@sapphire/decorators";
-import { Command, CommandOptions, container } from "@sapphire/framework";
+import { Command, CommandOptions } from "@sapphire/framework";
 import { reply } from "@sapphire/plugin-editable-commands";
-import type { GuildMember, Message } from "discord.js";
+import { Collection, GuildMember, Message } from "discord.js";
 import { emojis } from "../../assets/emojis.json";
+import { correct, incorrect, timeout } from "../../assets/guessEmojiMessages.json";
 
 @ApplyOptions<CommandOptions>({
     description: "guess the name of a randome emoji",
@@ -11,51 +12,66 @@ import { emojis } from "../../assets/emojis.json";
 export class UserCommand extends Command {
     private guildID!: string;
     private member!: GuildMember;
+    private streaks!: Collection<string, number>;
+    private streak!: number;
     private emoji!: string;
 
     public async messageRun(message: Message) {
         this.guildID = message.guild?.id!;
         this.member = message.member!;
+        this.streaks = this.container.guessEmojiStreak;
+        this.streak = this.streaks.has(this.guildID) ? this.streaks.get(this.guildID)! : 0;
+        this.emoji = emojis[~~(Math.random() * emojis.length)];
 
-        // im so sorry for everything oh god remind me to rewrite this
-        let streak = container.guessEmojiStreak.has(this.guildID)
-            ? container.guessEmojiStreak.get(this.guildID)
-            : container.guessEmojiStreak.set(this.guildID, 0).get(this.guildID);
+        const emojiMessage = await reply(message, `:${this.emoji}:`);
 
-        const randomEmoji = emojis[~~(Math.random() * emojis.length)];
-        const emojiMessage = await reply(message, `:${randomEmoji}:`);
-        this.emoji = randomEmoji;
+        const filter = (msg: Message) => msg.author.id === message.author.id;
 
-        const filter = (m: Message) => m.author.id === message.author.id;
-        try {
-            const collection = await message.channel.awaitMessages({ filter, time: 15000, max: 1, errors: ["time"] });
-            const replyMessage = collection.first()?.content.toLowerCase().replaceAll(" ", "_");
+        message.channel.awaitMessages({ filter, time: 15000, max: 1, errors: ["time"] })
+            .then((collection) => {
+                const response = collection.first()?.content.toLocaleLowerCase().replaceAll(" ", "_");
 
-            if (replyMessage === randomEmoji) return this.setStreakAndReply(streak! + 1, emojiMessage, "success");
-            this.setStreakAndReply(0, emojiMessage, "fail");
-        } catch (err: any) { this.setStreakAndReply(0, emojiMessage, "timeout"); }
+                if (response === this.emoji) {
+                    return this.setStreakAndReply(++this.streak!, emojiMessage, "correct");
+                }
+
+                return this.setStreakAndReply(0, emojiMessage, "incorrect");
+            })
+            .catch((err) => {
+                if (err instanceof Collection) {
+                    return this.setStreakAndReply(0, emojiMessage, "timeout");
+                }
+
+                this.container.logger.error(";Could not await messages in channel", err);
+            });
     }
 
-    private setStreakAndReply(streak: number, emojiMessage: Message, type: "success" | "fail" | "timeout") {
-        container.guessEmojiStreak.set(this.guildID, streak);
-        reply(emojiMessage, { content: this.member.toString(), embeds: [this.returnEmbed(streak!, type)] });
+    private setStreakAndReply(newStreak: number, message: Message, type: string) {
+        this.streaks.set(this.guildID, newStreak);
+
+        reply(message, {
+            content: this.member.toString(),
+            embeds: [this.returnEmbed(type, newStreak)]
+        });
     }
 
-    private returnEmbed(streak: number, type: "success" | "fail" | "timeout") {
+    private returnEmbed(type: string, streak: number) {
         let title;
 
-        if (type === "success") title = `correct emote!`;
-        else if (type === "fail") title = `wrong emote!\nit was \`${this.emoji}\``;
-        else title = `ran out of time!\nit was \`${this.emoji}\``;
+        if (type === "correct") {
+            title = correct[~~(Math.random() * correct.length)];
+        } else if (type === "incorrect") {
+            title = `${incorrect[~~(Math.random() * incorrect.length)]}\nit was \`${this.emoji}\``;
+        } else {
+            title = `${timeout[~~(Math.random() * timeout.length)]}\nit was \`${this.emoji}\``;
+        }
 
         return {
-            color: type === "success" ? 0x2ecc71 : 0xe74c3c,
-            author: {
-                name: this.member.nickname! ?? this.member.user.username,
-                icon_url: this.member?.user.avatarURL()!
-            },
+            color: type === "correct" ? 0x2ecc71 : 0xe74c3c,
             title,
-            description: `${type === "success" ? "current" : "lost"} streak: \`${streak}\``
-        };
+            description: type === "correct"
+                ? `streak: \`${streak}\``
+                : `lost streak: \`${this.streak}\``
+        }
     }
 }
